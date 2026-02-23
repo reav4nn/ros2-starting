d157dd3e3314bee5dec4ec17ee18a51a99885e5f
reav4nn <reav4nn@gmail.com>
2026-02-23 12:11:12 +0400

    Enhance timer callback to track and log call count

--- my_py_pkg/my_first_node.py
+++ my_py_pkg/my_first_node.py
@@ -6,11 +6,15 @@ from rclpy.node import Node
 class MyFirstNode(Node):
     def __init__(self):
         super().__init__('my_first_node')
+
+        self.counter = 0
         self.get_logger().info('Hello ROS2')
         self.timer = self.create_timer(1.0, self.timer_callback)
+
         
     def timer_callback(self):
-        self.get_logger().info('Timer callback called')
+        self.counter += 1
+        self.get_logger().info(f'Timer callback called {self.counter} times')
 
 def main(args=None):
     rclpy.init(args=args)
