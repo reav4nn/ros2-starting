42388fd290696c103d08d3f52d194744740b389f
reav4nn <reav4nn@gmail.com>
2026-02-23 13:43:43 +0400

    Refactor MyRobotStatePublisher to improve state publishing logic

--- my_py_pkg/robot_state_publisher.py
+++ my_py_pkg/robot_state_publisher.py
@@ -9,6 +9,14 @@ class MyRobotStatePublisher(Node):
         super().__init__('my_robot_state_publisher')
 
         self.publisher_ = self.create_publisher(String, 'robot_state', 10)
+        self.timer = self.create_timer(1.0, self.publish_state)
+
+        self.get_logger().info('MyRobotStatePublisher node has been started.')
+
+    def publish_state(self):
+        msg = String() # creating a new String message instance
+        msg.data = 'Robot state at time {}'.format(self.get_clock().now().to_msg()) # setting the data field of the message to a string that includes the current time
+        self.publisher_.publish(msg) # publishing the message to the 'robot_state' topic
 
 def main(args=None):
     rclpy.init(args=args)
